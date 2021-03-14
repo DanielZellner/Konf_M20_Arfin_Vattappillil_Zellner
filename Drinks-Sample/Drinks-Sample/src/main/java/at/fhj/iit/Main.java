081b7d92ab9ddbf1159c37f6a69141000fef65ec
Linda3108 <linda.arfin@edu.fh-joanneum.at>
2021-03-14 16:22:02 +0100

    I've changed the main method

--- Drinks-Sample/Drinks-Sample/src/main/java/at/fhj/iit/Main.java
+++ Drinks-Sample/Drinks-Sample/src/main/java/at/fhj/iit/Main.java
@@ -6,14 +6,19 @@ public class Main {
 
         Liquid l = new Liquid("Wein", 0.125, 13);
         Liquid w = new Liquid("Whiskey", 0.125, 40);
+	Liquid v = new Liquid("Vodka", 0.234, 30);
         System.out.println(l.getName());
         System.out.println(l.getVolume());
         System.out.println(w.getName());
         System.out.println(w.getVolume());
+	System.out.println(v.getName());
+        System.out.println(v.getVolume());
 
         Drink d = new SimpleDrink("Rotwein",l);
         Drink wCola = new Cocktails("Jack Daniels",w);
         System.out.println(d);
         System.out.println(wCola);
+        Drink d1 = new SimpleDrink("Vodka",v);
+        System.out.println(d1);
     }
 }
