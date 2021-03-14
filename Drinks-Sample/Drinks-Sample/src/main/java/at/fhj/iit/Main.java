8d753682e36c993e4f8a2d5d4d744ed9ff9019a7
Daniel Zellner <daniel.zellner@edu.fh-joanneum.at>
2021-03-14 15:40:53 +0100

    3. commit: Main und Cocktail class upgedated um eine Ausgabe zu haben

--- Drinks-Sample/Drinks-Sample/src/main/java/at/fhj/iit/Main.java
+++ Drinks-Sample/Drinks-Sample/src/main/java/at/fhj/iit/Main.java
@@ -5,10 +5,15 @@ public class Main {
     public static void main(String[] args){
 
         Liquid l = new Liquid("Wein", 0.125, 13);
+        Liquid w = new Liquid("Whiskey", 0.125, 40);
         System.out.println(l.getName());
         System.out.println(l.getVolume());
+        System.out.println(w.getName());
+        System.out.println(w.getVolume());
 
         Drink d = new SimpleDrink("Rotwein",l);
+        Drink wCola = new Cocktails("Jack Daniels",w);
         System.out.println(d);
+        System.out.println(wCola);
     }
 }
